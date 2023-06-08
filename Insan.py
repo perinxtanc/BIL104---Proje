4e7d0c9ddb4fc236d985ad7c4554768c3bd6786b
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 19:29:30 +0300

    Create object in Insan.py

--- Insan.py
+++ Insan.py
@@ -1,1 +1,8 @@
-
+class Insan:
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk):
+        self.__tc_no = tc_no
+        self.__ad = ad
+        self.__soyad = soyad
+        self.__yas = yas
+        self.__cinsiyet = cinsiyet
+        self.__uyruk = uyruk
