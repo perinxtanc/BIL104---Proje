804ddb98e2ff5cbf6bb12f7c7f12992de253f018
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 20:47:52 +0300

    Create object in Issiz.py

--- Issiz.py
+++ Issiz.py
@@ -1,1 +1,7 @@
 from Insan import Insan
+
+class Issiz(Insan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, tecrube):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
+        self.__tecrube = tecrube
+        self.__statu = self.__statu_bul(tecrube)
