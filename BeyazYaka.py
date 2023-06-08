2f8d8eae80b5055f8dac9c9b8f7127c54cd0ed7c
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 22:23:44 +0300

    Add method to calculate the entitlement for salary increase based on experience in BeyazYaka.py

--- BeyazYaka.py
+++ BeyazYaka.py
@@ -10,3 +10,17 @@ class BeyazYaka(Calisan):
     
     def set_tesvik_primi(self, tesvik_primi):
         self.__tesvik_primi = tesvik_primi
+    
+    def zam_hakki(self):
+        try:
+            tecrube_sene = int(self.get_tecrube().split()[0])
+            if tecrube_sene < 2:
+                return self.__tesvik_primi
+            elif tecrube_sene <= 4 and self.get_maas() < 15000:
+                return (self.get_maas() % tecrube_sene) * 5 + self.__tesvik_primi
+            elif tecrube_sene > 4 and self.get_maas() < 25000:
+                return (self.get_maas() % tecrube_sene) * 4 + self.__tesvik_primi
+            else:
+                return 0
+        except:
+            return 0
