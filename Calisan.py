ce4f3268c96a56b840d9685b32c95d37efacb9f5
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 21:06:40 +0300

    Create object in Calisan.py

--- Calisan.py
+++ Calisan.py
@@ -1,1 +1,8 @@
 from Insan import Insan
+
+class Calisan(Insan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
+        self.__sektor = sektor
+        self.__tecrube = tecrube
+        self.__maas = maas
