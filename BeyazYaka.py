9e700a8963d8dcd02839abf29db7084f1ef01dc7
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 22:01:31 +0300

    Get tesvik_primi in BeyazYaka.py

--- BeyazYaka.py
+++ BeyazYaka.py
@@ -4,3 +4,6 @@ class BeyazYaka(Calisan):
     def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, tesvik_primi):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
         self.__tesvik_primi = tesvik_primi
+    
+    def get_tesvik_primi(self):
+        return self.__tesvik_primi
