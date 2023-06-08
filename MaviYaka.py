33eaec95d9c864331ffcb1cc7ebc8a285dca8433
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 21:28:40 +0300

    Get yipranma_payi in MaviYaka.py

--- MaviYaka.py
+++ MaviYaka.py
@@ -4,3 +4,6 @@ class MaviYaka(Calisan):
     def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, yipranma_payi):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
         self.__yipranma_payi = yipranma_payi
+            
+    def get_yipranma_payi(self):
+        return self.__yipranma_payi
