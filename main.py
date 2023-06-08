93de55dea375cc69fc1b5178dcc9d6770f100e91
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 22:30:19 +0300

    Create object of Insan class in main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from Calisan import Calisan
 from BeyazYaka import BeyazYaka
 from MaviYaka import MaviYaka
 from Issiz import Issiz
+
+insan1 = Insan('11111111111', 'Ahmet', 'Yılmaz', 30, 'Erkek', 'Türk')
+insan2 = Insan('22222222222', 'Ayşe', 'Demir', 25, 'Kadın', 'Türk')
