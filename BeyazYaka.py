6930be2737767c156e97d20cb466d465ccb04ea9
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 21:48:12 +0300

    Create object in BeyazYaka.py

--- BeyazYaka.py
+++ BeyazYaka.py
@@ -1,1 +1,6 @@
 from Calisan import Calisan
+
+class BeyazYaka(Calisan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, tesvik_primi):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
+        self.__tesvik_primi = tesvik_primi
