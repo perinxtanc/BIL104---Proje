2b1d729608134cd081e7bdd637dcad8c8b47f6c8
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 22:31:31 +0300

    Create object of Calisan class in main.py

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ from Issiz import Issiz
 
 insan1 = Insan('11111111111', 'Ahmet', 'Yılmaz', 30, 'Erkek', 'Türk')
 insan2 = Insan('22222222222', 'Ayşe', 'Demir', 25, 'Kadın', 'Türk')
+
+calisan1 = Calisan('33333333333', 'Mehmet', 'Aydın', 35, 'Erkek', 'Türk', 'teknoloji', 60, 20000)
+calisan2 = Calisan('44444444444', 'Fatma', 'Kaya', 28, 'Kadın', 'Türk', 'muhasebe', 36, 15000)
+calisan3 = Calisan('55555555555', 'Ali', 'Şahin', 32, 'Erkek', 'Türk', 'inşaat', 84, 25000)
