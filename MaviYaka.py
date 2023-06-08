0b6ef3fda4c5a4084841ac10044b3748e1602380
perinxtanc <128799447+perinxtanc@users.noreply.github.com>
2023-06-08 21:22:16 +0300

    Update MaviYaka.py

--- MaviYaka.py
+++ MaviYaka.py
@@ -1,1 +1,6 @@
 from Calisan import Calisan
+
+class MaviYaka(Calisan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, yipranma_payi):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
+        self.__yipranma_payi = yipranma_payi
